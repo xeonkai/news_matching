d60367c3750bcfa9509fd618cd4b2d6b5bb7050f
Yu Kai <shinco.chong@gmail.com>
2022-08-26 00:19:11 +0800

    update topic discovery structure

--- app/pages/02_Topic Discovery.py
+++ app/pages/02_Topic Discovery.py
@@ -1,25 +1,14 @@
 import streamlit as st
 import time
-import os
 
-#os.chdir("/Users/shreya/Documents/govtech/news_matching")
 
 from pathlib import Path
 import pandas as pd
 from top2vec import Top2Vec
-import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import softmax
 from wordcloud import WordCloud
 
-#raw_summ_path = Path("scripts", "summarisation.py")
-
-
-#from ..scripts.summarisation import summarise_text
-
-#from summarisation import summarise_text
-
-#streamlit run "/Users/shreya/Documents/govtech/news_matching/app/pages/02_Article Discovery.py" 
 
 st.title("Topic Discovery")
 st.sidebar.markdown("# Settings")
@@ -30,91 +19,81 @@ start_time = time.perf_counter()
 # User input: number of clusters - to modify granularity: topic_hierarchy_func
 # Eventually: a few news clusters in order of engagement (breaking news)
 
-raw_data_path = Path("data", "raw", "SG sanctions on Russia.xlsx")
 
 @st.cache()
-def load_news_data():
-    #SET TO UKRAINE ARTICLES FOR NOW
-    df = (
-        pd.read_excel(
-            raw_data_path,
-            sheet_name="Contents",
-            parse_dates=["date"],
-            usecols=[
-                "id",
-                "source",
-                "title",
-                "content",
-                "date",
-                "url",
-                "domain",
-            ]
-        ).set_index("id") 
-    )[lambda df: df["source"] == "Online News"]
+def load_news_data(data_path):
+    df = pd.read_parquet(data_path)[lambda df: df["source"] == "Online News"]
     return df.copy(), df["title"].to_list(), df["content"].to_list()
 
-df, titles, content = load_news_data()
 
-st.sidebar.selectbox(
+data_path = Path("data", "processed", "sg_sanctions_on_russia.parquet")
+df, titles, content = load_news_data(data_path)
+
+title_or_content = st.sidebar.selectbox(
     "Compare Title or Content",
     (
-        "Title",
         "Content",
+        "Title",
     ),
 )
 
-st.file_uploader('Upload excel file here') #NOT LINKED YET
+st.file_uploader("Upload excel file here")  # NOT LINKED YET
+
 
 @st.cache()
-def load_model(chosen_model):
-    if chosen_model=="Top2Vec":
-        if Path("scripts/top2vec").is_file():
-            model = Top2Vec.load("scripts/top2vec")
-        else:
-            model = Top2Vec(documents=content, speed="learn", workers=8) #Top2Vec(documents=list(data["content"]), speed="learn", workers=8)
-    return model 
+def load_model(checkpoint, title_or_content):
+    if checkpoint == "Top2Vec":
+        model_path = Path("scripts", f"top2vec_{title_or_content.lower()}")
+        if model_path.is_file():
+            model = Top2Vec.load(model_path)
+    return model
 
-selected_model = st.sidebar.selectbox("Model Type", ['Top2Vec'])
-model = load_model(selected_model)
+
+selected_model = st.sidebar.selectbox("Model Type", ["Top2Vec"])
+model = load_model(selected_model, title_or_content)
 
 st.header("Top News Topics")
 
+
 def wordcloud_generator(mdl, num_topic, reduced=False):
     model._validate_topic_num(num_topic, reduced)
-    word_score_dict = dict(zip(mdl.topic_words[num_topic], softmax(mdl.topic_word_scores[num_topic])))
-    cloud = WordCloud(width=1600,
-                      height=400,
-                      background_color='black').generate_from_frequencies(word_score_dict)
+    word_score_dict = dict(
+        zip(mdl.topic_words[num_topic], softmax(mdl.topic_word_scores[num_topic]))
+    )
+    cloud = WordCloud(
+        width=1600, height=400, background_color="black"
+    ).generate_from_frequencies(word_score_dict)
     return cloud
+
 
 def top_n_wordclouds(n, a):
     for topicnum in range(n):
 
         wc = wordcloud_generator(model, topicnum)
-        fig, ax = plt.subplots(figsize = (16, 4))
+        fig, ax = plt.subplots(figsize=(16, 4))
         ax.imshow(wc)
         plt.axis("off")
-        plt.title("Topic " + str(topicnum+1), loc='left', fontsize=30, pad=20)
+        plt.title("Topic " + str(topicnum + 1), loc="left", fontsize=30, pad=20)
         with st.container():
             st.pyplot(fig)
-            txt = list(model.search_documents_by_topic(topic_num=topicnum, num_docs=a))[0]
-            st.markdown("#### " + "Sample articles from Topic " + str(topicnum+1))
+            txt = list(model.search_documents_by_topic(topic_num=topicnum, num_docs=a))[
+                0
+            ]
+            st.markdown("#### " + "Sample articles from Topic " + str(topicnum + 1))
             for articlenum in range(a):
-                with st.expander("Article " + str(articlenum+1)):
+                with st.expander("Article " + str(articlenum + 1)):
                     st.markdown("##### Summary of Article")
                     st.markdown("SUMMARISED TEXT PLACEHOLDER")
                     st.markdown("##### Full Article:")
                     st.markdown(txt[articlenum])
-            st.text(" "); st.text(" "); st.text(" ")  
+            st.text(" ")
+            st.text(" ")
+            st.text(" ")
 
 
 j = int(
     st.sidebar.slider(
-        label="How many articles?",
-        min_value=1,
-        value=3,
-        max_value=30,
-        step=1
+        label="How many articles?", min_value=1, value=3, max_value=30, step=1
     )
 )
 
@@ -123,21 +102,9 @@ k = int(
         label="How many topics?",
         min_value=1,
         value=5,
-        max_value = 30,
+        max_value=30,
         step=1,
     )
 )
 
-#def discover_top_news_topics():
-
-
-
-
-
 top_n_wordclouds(k, j)
-
-
-
-
-
-
